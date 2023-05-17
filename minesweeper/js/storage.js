26457ea9fb27704ea647b6502f450806b08baeaf
Vitaly Kukushkin <phoinixru@gmail.com>
2023-05-17 13:58:03 +0200

    feat: implement storage class

---
+++ minesweeper/js/storage.js
@@ -0,0 +1,37 @@
+const { stringify, parse } = JSON;
+const STORAGE_NAME = 'phoinixru';
+
+export default class Storage {
+  #name;
+
+  #data;
+
+  constructor(name = STORAGE_NAME) {
+    this.#name = name;
+
+    this.load();
+  }
+
+  set(prop, value) {
+    this.#data[prop] = value;
+    this.save();
+  }
+
+  get(prop) {
+    return this.#data[prop] || null;
+  }
+
+  load() {
+    let storedData;
+    try {
+      storedData = parse(localStorage.getItem(this.#name));
+    } catch (e) {
+      storedData = {};
+    }
+    this.#data = storedData;
+  }
+
+  save() {
+    localStorage.setItem(this.#name, stringify(this.#data));
+  }
+}
