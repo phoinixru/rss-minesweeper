c5be4127ebb8e94a8bb931cf604ff586de92edb0
Vitaly Kukushkin <phoinixru@gmail.com>
2023-05-26 11:41:31 +0200

    feat: add section  with advanced moves to help

--- minesweeper/js/help.js
+++ minesweeper/js/help.js
@@ -4,6 +4,7 @@ import Button from './button.js';
 
 const CssClasses = {
   COMPONENT: 'help',
+  FIELD: 'field',
 };
 
 const cell = ({
@@ -23,6 +24,40 @@ const cell = ({
   return el.outerHTML;
 };
 
+const field = (cells) => {
+  const size = Math.sqrt(cells.length);
+  const el = elt('div', { className: CssClasses.FIELD });
+  el.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  el.innerHTML = cells.map(cell).join`\n`;
+
+
+  return el.outerHTML;
+}
+
+const open = true;
+const flagged = true;
+const FIELD_EIGHT = field([
+  {}, {}, {},
+  {}, { mines: 8, open }, {},
+  {}, {}, {},
+]);
+const FIELD_EIGHT_CLICKED = field([
+  { flagged }, { flagged }, { flagged },
+  { flagged }, { mines: 8, open }, { flagged },
+  { flagged }, { flagged }, { flagged },
+]);
+
+const FIELD_FOUR = field([
+  { flagged }, {  }, { flagged },
+  {  }, { mines: 4, open }, {  },
+  { flagged }, {  }, { flagged },
+]);
+const FIELD_FOUR_CLICKED = field([
+  { flagged }, { mines: 2, open }, { flagged },
+  { mines: 2, open }, { mines: 4, open }, { mines: 2, open },
+  { flagged }, { mines: 2, open }, { flagged },
+]);
+
 const HELP_HTML = `
 <p>Morning/afternoon/eve.<br>
 Your mission, should you choose to accept it, is to open all the empty cells while avoiding cells with mines.</p>
@@ -34,9 +69,16 @@ Your mission, should you choose to accept it, is to open all the empty cells whi
 
 <p>To win you have to open all empty cells.</p>
 
-<p>Hints:<br>
-- if the number of flagged cells around open cell matches the number inside the cell, you can click it to reveal content cells around<br>
-- struggle with hard level - try to use The <strong>K</strong>code.</p>`;
+<h2>Advanced moves:</h2>
+<p>If you have marked all the bombs around open cell, you can click it to reveal contents of the rest of the cells, but be wary:<br>
+<div class="example">${FIELD_FOUR} -> ${FIELD_FOUR_CLICKED}</div>
+</p>
+<p>if the number of closed cells around open cell matches the number inside the cell, you can click it to mark these cells with a flag:<br>
+<div class="example">${FIELD_EIGHT} -> ${FIELD_EIGHT_CLICKED}</div>
+</p>
+
+<p>Struggle with hard level? - try to use The <strong>Konami</strong> Code:<br>
+↑↑↓↓←→←→BA + Enter</p>`;
 
 export default class Help {
   constructor({ container }) {
